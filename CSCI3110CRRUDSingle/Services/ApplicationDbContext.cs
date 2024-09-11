ceff7ed8f5f9229983b9b9783fd9956f6031009b
Roach, Jeff <ROACHJ@ETSU.EDU>
2024-09-11 15:56:38 -0400

    Add project files.

---
+++ CSCI3110CRRUDSingle/Services/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CSCI3110CRRUDSingle.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSCI3110CRRUDSingle.Services;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Person> People => Set<Person>();
+}
+
